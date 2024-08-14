fe73747745299f522a268ce9fbabf3ed384be5c8
Francisco Scavassa <chicopsych@hotmail.com>
2024-08-14 13:32:05 -0300

    sincronizando repositorio

---
+++ csharp/main.cs
@@ -0,0 +1,7 @@
+/* Program para implementar um protetor de tela para usuarios windows da rede local
+  Autor: Francisco Luvisari Scavassa
+  E-mail: francisco.lscavassa@proton.me
+  Data: 2024/08/14
+  Versão: 1.0 pre-alpha
+*/
+
